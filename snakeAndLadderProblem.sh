f36ba239924490365ec0da74cc8e526c6adf7535
1301mohit <mohitkr1301@gmail.com>
2019-11-15 19:19:22 +0530

    Add two player in snake and ladder game

--- snakeAndLadderProblem.sh
+++ snakeAndLadderProblem.sh
@@ -4,7 +4,7 @@ echo Welcome to Snake and Ladder Simulator
 
 
 STARTING_POSITION=0
-WINNING_POSITION=100
+WINNING_POSITION=10
 NO_PLAY=0
 LADDER=1
 SNAKE=2
@@ -13,43 +13,63 @@ PLAYER2=1
 
 lastPosition=0
 position=0
+position1=0
+position2=0
 option=0
-countWin=0
+countWin1=0
+countWin2=0
 player=0
 
-while [ $position -lt $WINNING_POSITION ]
-do
+function play()
+{
 	checkDie=$((RANDOM % 6 + 1))
 	option=$((RANDOM % 3))
-	((countWin++))
 	case $option in
 	$NO_PLAY)
-		position=$position
+		position=$1
+		echo $position
 		;;
 	$LADDER)
-		lastPosition=$position
-		position=$(($position+$checkDie))
+		lastPosition=$1
+		position=$(($1+$checkDie))
 		if [ $position -gt $WINNING_POSITION ]
 		then
 			position=$lastPosition
 		fi
+		echo $position
 		;;
 	$SNAKE)
-		position=$(($position-$checkDie))
+		position=$(($1-$checkDie))
 		if [ $position -lt $STARTING_POSITION ]
 		then
 			position=0
 		fi
+		echo $position
 		;;
 	esac
+}
+
+while [ $position1 -lt $WINNING_POSITION ] || [ $position2 -lt $WINNING_POSITION ]
+do
 	if [ $player -eq $PLAYER1 ]
 	then
+		((countWin1++))
+		position1="$( play $position1 )"
 		player=$PLAYER2
 	else
+		((countWin2++))
+		position2="$( play $position2 )"
 		player=$PLAYER1
 	fi
 done
-echo "Winning count:"$countWin
-echo "Winning player:"$player
+
+if [ $player -eq $PLAYER1 ]
+then
+	echo "Winning count:"$countWin1
+else
+	echo "Winning Count:"$countWin2
+fi
+
+echo "Winning player: Player"$player
 
 
