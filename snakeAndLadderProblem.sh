fc1e65fa6dec8c5d8e01d895ba1cce3c2c37d07d
1301mohit <mohitkr1301@gmail.com>
2019-11-15 11:55:09 +0530

    Add the winning position 100

--- snakeAndLadderProblem.sh
+++ snakeAndLadderProblem.sh
@@ -4,7 +4,8 @@ echo Welcome to Snake and Ladder Simulator
 
 position=0
 option=0
-checkDie=$((RANDOM % 6 + 1))
+startingPosition=0
+winningPosition=100
 noPlay=0
 ladder=1
 snake=2
@@ -15,45 +16,29 @@ die4=4
 die5=5
 die6=6
 
-function snakeAndLadder() {
+while [ $position -lt $winningPosition ]
+do
+	checkDie=$((RANDOM % 6 + 1))
 	option=$((RANDOM % 3))
 	case $option in
-		$noPlay)
-			position=$position
-			;;
-		$ladder)
-			position=$(($position+$checkDie))
-			;;
-		$snake)
-			position=$(($position-$checkDie))
-			if [ $position -lt 0 ]
-			then
-				position=0
-			fi
-			;;
+	$noPlay)
+		position=$position
+		;;
+	$ladder)
+		position=$(($position+$checkDie))
+		if [ $position -gt $winningPosition ]
+		then
+			position=100
+		fi
+		;;
+	$snake)
+		position=$(($position-$checkDie))
+		if [ $position -lt $startingPosition ]
+		then
+			position=0
+		fi
+		;;
 	esac
-}
+done
 
 
-case $checkDie in
-	$die1)
-		echo $die1
-		;;
-	$die2)
-		echo $die2
-		;;
-	$die3)
-		echo $die3
-		;;
-	$die4)
-		echo $die4
-		;;
-	$die5)
-		echo $die5
-		;;
-	*)
-		echo $die6
-		;;
-esac
-
-snakeAndLadder
